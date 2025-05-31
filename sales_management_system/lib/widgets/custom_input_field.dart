360aa13653cfe5252a6342c3b3d22e8b16418bd0
lurnalu <byreswill901@gmail.com>
2025-05-31 22:51:47 +0300

    Add sales_management_system/lib/widgets/custom_input_field.dart 1748721107279

--- sales_management_system/lib/widgets/custom_input_field.dart
+++ sales_management_system/lib/widgets/custom_input_field.dart
@@ -8,6 +8,8 @@ class CustomInputField extends StatelessWidget {
   final String? Function(String?)? validator;
   final TextInputType keyboardType;
   final bool autofocus;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
 
   const CustomInputField({
     Key? key,
@@ -18,10 +20,14 @@ class CustomInputField extends StatelessWidget {
     this.validator,
     this.keyboardType = TextInputType.text,
     this.autofocus = false,
+    this.prefixIcon,
+    this.suffixIcon,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextFormField(
@@ -30,17 +36,52 @@ class CustomInputField extends StatelessWidget {
         validator: validator,
         keyboardType: keyboardType,
         autofocus: autofocus,
+        style: theme.textTheme.bodyLarge,
         decoration: InputDecoration(
           labelText: label,
           hintText: hint,
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: theme.primaryColor,
+              width: 2,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: theme.primaryColor.withOpacity(0.5),
+              width: 1,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: theme.primaryColor,
+              width: 2,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: theme.colorScheme.error,
+              width: 1,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: theme.colorScheme.error,
+              width: 2,
+            ),
           ),
           filled: true,
-          fillColor: Theme.of(context).colorScheme.surface,
+          fillColor: theme.cardColor,
           contentPadding: const EdgeInsets.symmetric(
             horizontal: 16,
-            vertical: 12,
+            vertical: 16,
           ),
         ),
       ),
