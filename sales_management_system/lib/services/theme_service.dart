d7e7067f05aca5bef87ca28d38a8fcb8f7e8b97e
lurnalu <byreswill901@gmail.com>
2025-05-31 22:51:52 +0300

    Add sales_management_system/lib/services/theme_service.dart 1748721111931

--- sales_management_system/lib/services/theme_service.dart
+++ sales_management_system/lib/services/theme_service.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeService extends ChangeNotifier {
   static const String _darkModeKey = 'darkMode';
-  final SharedPreferences? _prefs;
+  SharedPreferences? _prefs;
   bool _isDarkMode = false;
 
-  ThemeService() : _prefs = null {
+  ThemeService() {
     _loadThemeMode();
   }
 
   bool get isDarkMode => _isDarkMode;
 
   Future<void> _loadThemeMode() async {
-    final prefs = await SharedPreferences.getInstance();
-    _isDarkMode = prefs.getBool(_darkModeKey) ?? false;
+    _prefs = await SharedPreferences.getInstance();
+    _isDarkMode = _prefs?.getBool(_darkModeKey) ?? false;
     notifyListeners();
   }
 
   Future<void> toggleTheme() async {
-    final prefs = await SharedPreferences.getInstance();
+    _prefs = await SharedPreferences.getInstance();
     _isDarkMode = !_isDarkMode;
-    await prefs.setBool(_darkModeKey, _isDarkMode);
+    await _prefs?.setBool(_darkModeKey, _isDarkMode);
     notifyListeners();
   }
 }
